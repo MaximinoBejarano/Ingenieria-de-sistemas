3db3223e85137e25528b06f98f0be7e753069dcd
MaximinoBejarano <32820919+MaximinoBejarano@users.noreply.github.com>
2018-03-27 21:21:06 -0600

    Max_Cambios
    Fxml_AbonosController,AbonosJPAController

--- Ferreteria_Las_Vegas/src/ferreteria_las_vegas/model/controller/AbonosJPAController.java
+++ Ferreteria_Las_Vegas/src/ferreteria_las_vegas/model/controller/AbonosJPAController.java
@@ -4,10 +4,14 @@
  * and open the template in the editor.
  */
 package ferreteria_las_vegas.model.controller;
+import ferreteria_las_vegas.model.entities.Abono;
 
 import ferreteria_las_vegas.utils.EntityManagerHelper;
+import java.util.List;
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 /**
  *
@@ -37,4 +41,66 @@ public class AbonosJPAController {
 
     private EntityManager em = EntityManagerHelper.getInstance().getManager();
     private EntityTransaction et;
+    
+    //***************************** Area de Metodos***********************************
+    /**
+     * Se agrega un nuevo registro a la base de datos
+     * @param Abono
+     * @return 
+     */
+    public Abono AgregarAbono(Abono Abono) {
+        et = em.getTransaction();
+        try {
+            et.begin();
+            em.persist(Abono);
+            et.commit();
+            return Abono;
+        } catch (EntityExistsException ex) {
+            et.rollback();
+            System.err.println(ex);
+            return null;
+        } catch (Exception ex) {
+            et.rollback();
+            System.err.println(ex);
+            return null;
+        }
+    }
+    
+     /**
+     * Metodo para realizar la modificación de un registro de abono
+     *
+     * @param pAbono 
+     * @return
+     */
+    public Abono ModificarAbono (Abono pAbono) {
+        et = em.getTransaction();
+        try {
+            et.begin();
+            em.merge(pAbono);
+            et.commit();
+            return pAbono;
+        } catch (EntityExistsException ex) {
+            et.rollback();
+            System.err.println(ex);
+            return null;
+        } catch (Exception ex) {
+            et.rollback();
+            System.err.println(ex);
+            return null;
+        }
+    }
+     /**
+     * Se realiza la consulta todos los abonos
+     *
+     * @return
+     */
+    public List<Abono> ConsultarAbonos() {
+        try {
+            Query qry = em.createNamedQuery("Abono.findAll", Abono.class);// consulta todos los abonos
+            List<Abono> Abonos = qry.getResultList();// Recibe el resultado de la consulta  
+            return Abonos;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
 }
